33b221cdc3cfc2778a82c145cf3c58921b36de5e
mintimati <mintimati@gmail.com>
2023-03-09 00:07:27 +0100

    Added comments.

--- baggageColors/src/Solution.java
+++ baggageColors/src/Solution.java
@@ -7,18 +7,18 @@ import java.util.regex.Pattern;
 public class Solution {
 
 	public static void main(String[] args) {
-
 		Solution.baggageColors("baggageColors/src/inputfile");
-
 	}
 
 
 	public static void baggageColors(String path) {
+		//accessing the "inputfile" via stream
 		try {
 			var amount = Files
 					.lines(Paths.get(path))
 					.filter(e -> e.contains("shiny"))
 					.map(e -> {
+						// we look for shiny gold bags, with MAYBE an int before them
 						String patt = "\\d?\\s?shiny gold bag[s]?";
 						Pattern r = Pattern.compile(patt);
 						Matcher m = r.matcher(e);
@@ -27,6 +27,18 @@ public class Solution {
 						}
 						return e;
 					})
+
+					/*
+					we turn the results, in this case the following:
+					1 shiny gold bag
+					shiny gold bag
+					2 shiny gold bag
+
+					into stringbuilders so they're mutable, and then we reduce them to their
+					first character, so we can learn how many of them are there per line (if there
+					is no integer, we must assume there is only one.
+					 */
+
 					.map(e -> new StringBuilder(e.toString()))
 					.map(e -> e.delete(1, e.length()))
 					.map(e -> {
@@ -38,6 +50,7 @@ public class Solution {
 							return 1;
 						}
 					})
+					//summing the amount of them up
 					.reduce(0, (subtotal, element) -> subtotal + element);
 
 			System.out.println(amount);
